f75fdd09c08710194e89e56cb0a377eb249d4c46
Abed Al Rahman Itani <Abed.Itani@hedgeguard.com>
2023-09-27 22:41:54 +0100

    Solved - Hacker Rank Super Reduced String

--- MyAlgos/HackerRank/SuperReducedString.cs
+++ MyAlgos/HackerRank/SuperReducedString.cs
@@ -1,40 +1,64 @@
-﻿namespace HackerRank;
+﻿
+using System.Text;
+
+namespace HackerRank;
 
 internal class SuperReducedString
 {
     static void Main(string[] args)
     {
-        var result = superReducedString("zztqooauhujtmxnsbzpykwlvpfyqijvdhuhiroodmuxiobyvwwxupqwydkpeebxmfvxhgicuzdealkgxlfmjiucasokrdznmtlwh");
+        var result = superReducedString("aaabccddd");
 
         Console.WriteLine(result.ToString());
         Console.WriteLine("Press enter to close...");
         Console.ReadLine();
     }
 
+    //public static string superReducedString(string s)
+    //{
+    //    var result = string.Empty;
+    //    var charsCountDico = new Dictionary<char, int>();
+
+    //    for (int i = 0; i < s.Length; i++)
+    //    {
+    //        if (charsCountDico.ContainsKey(s[i]))
+    //        {
+    //            charsCountDico[s[i]]++;
+    //        }
+    //        else
+    //        {
+    //            charsCountDico.Add(s[i], 1);
+    //        }
+    //    }
+
+    //    foreach (var kvp in charsCountDico)
+    //    {
+    //        if (kvp.Value % 2 != 0)
+    //        {
+    //            result += kvp.Key;
+    //        }
+    //    }
+
+    //    return result == string.Empty ? "Empty String" : result;
+    //}
+
     public static string superReducedString(string s)
     {
-        var result = string.Empty;
-        var charsCountDico = new Dictionary<char, int>();
+        var stack = new Stack<char>();
 
         for (int i = 0; i < s.Length; i++)
         {
-            if (charsCountDico.ContainsKey(s[i]))
+            if (stack.Count > 0 && s[i] == stack.Peek())
             {
-                charsCountDico[s[i]]++;
+                stack.Pop();
             }
             else
             {
-                charsCountDico.Add(s[i], 1);
+                stack.Push(s[i]);
             }
         }
 
-        foreach(var kvp in charsCountDico)
-        {
-            if(kvp.Value % 2 != 0) 
-            {
-                result += kvp.Key;
-            }
-        }
+        var result = new string(stack.Reverse().ToArray());
 
         return result == string.Empty ? "Empty String" : result;
     }
